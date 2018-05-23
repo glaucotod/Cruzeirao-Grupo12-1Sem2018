78c1fb01bbfc2041efbb155c3d269066ad7cdc11
Galhardo <Galhardo@172.18.3.173>
2018-05-22 22:20:10 -0300

    UserMB

---
+++ src/main/java/ManagedBeanF/UsuarioMB.java
@@ -0,0 +1,46 @@
+package MenagedBean;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import Classes.Usuario;
+
+
+
+@ManagedBean
+@SessionScoped
+
+public class UsuarioMB
+{
+	private Usuario user = new Usuario();
+	
+	public Usuario salvar (Usuario user)
+	{
+		user = userDAO.save(user);
+		userDAO.closeEntityManager();
+		return user;
+	}
+	
+	public List<Usuario> getUsuario()
+	{
+		List <Usuario> list = userDAO.getall(Usuario.class);
+		userDAO.closeEntityManager();
+		return list;
+	}
+	
+	public void alterar (Usuario user)
+	{
+		userDAO.save(user);
+		userDAO.closeEntityManager();
+	}
+	
+	public void remover (Usuario user)
+	{
+		user = userDAO.getById(Usuario.class, user.getIdUsuario());
+		userDAO.remove(user);
+		userDAO.closeEntityManager();
+	}
+	
+}
